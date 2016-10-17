db3a208de0c560d14dcd2c4573c355bad28d3543
wendell.pereira <wendell.pereira@LAB1-DCX-41.dcx.ufpb.br>
2016-10-17 09:40:19 -0300

    Add getters e setters

--- src/firstPacote/ClasseRaimundoCardoso.java
+++ src/firstPacote/ClasseRaimundoCardoso.java
@@ -4,4 +4,17 @@ public class ClasseRaimundoCardoso {
 	
 	private int idade = 25;
 	private String nome;
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
 }
